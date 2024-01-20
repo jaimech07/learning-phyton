278ccea25c905642c5c6eabe0fcbfda0f425f583
jaime c <chilamajaime12@gmail.com>
2024-01-20 08:16:36 -0500

    script phyton de una calculadora

---
+++ basic_calc.py
@@ -0,0 +1,37 @@
+#script title: Basic Calculator v1
+'''developer name: jaime a. chilama
+date: 18-01-2024'''
+
+
+''' description: 
+this calculator let you get the result of 4 basic
+math operations from two numbers(variables)
+'''
+#inputs (two static variables)
+num1 = 20 
+num2 = 5
+
+#process
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#outputs
+print("::: basic calc v1 :::")
+print("number 1:",num1,type(num1))
+print("number 2:",num2,type(num2))
+print("the addition is: ",add,type(add))
+print("the subtraction is: ",sub,type(sub))
+print("the multiplication is: ",mul,type(mul))
+print("the division is:", div,type(div))
+
+
+print("::: basic calc v1 :::")
+print("number 1:",num1,type(num1))
+print("number 2:",num2,type(num2))
+print("the addition is: ", num1 + num2)
+print("the subtraction is: ",num1 - num2)
+print("the multiplication is: " ,num1 * num2)
+print("the division is:", num1 / num2)
+
